0426165f3e9d39def4c5f8e9ed6dcb1dc137e03f
Belzito-crip <israel_v_de-jesus@estudante.sesisenai.org.br>
2025-10-29 02:12:44 -0300

    Atualiza método da API para obter logs e adiciona novo arquivo de log com operações matemáticas

--- ws_client.js
+++ ws_client.js
@@ -3,8 +3,8 @@ const dados = JSON.stringify({a: 18, b: 0});
 const payload = {
     hostname : "localhost",
     port     : 3000,
-    path     : "/dividir",
-    method   : "POST",
+    path     : "/logs",
+    method   : "GET",
     Headers  : {"Content-Type":"application/json", "Content-Lenght":Buffer.byteLength(dados)}
 };
 
@@ -12,7 +12,17 @@ const request = http.request(payload, response => {
     let resposta_txt = "";
     response.on("data", chunk => resposta_txt += chunk);
     response.on("end", () => {
-        console.log(`Resposta recebida: ${resposta_txt}`)
+        try {
+        // Tenta converter o texto da resposta em JSON
+        const json = JSON.parse(resposta_txt);
+
+        // Exibe formatado com 2 espaços de indentação
+        console.log("Resposta formatada:");
+        console.log(JSON.stringify(json, null, 2));
+    } catch (erro) {
+        // Se não for JSON válido, mostra o texto cru
+        console.log("Resposta recebida (texto):", resposta_txt);
+    }
     });
 });
 
